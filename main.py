ee885727a47092f06a6cf0a291681426f782daeb
komkritl <komkrit.wl@gmail.com>
2025-04-08 14:12:33 +0700

    add main.py

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 # https://www.youtube.com/watch?v=G4vH5IqLvDI
 # /UDjSSq,WW32a36
+from typing import Annotated
 
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import Depends, FastAPI, HTTPException, Query
+from datetime import datetime
+from sqlmodel import Field, Session, SQLModel, create_engine, select
 
-class Notification(BaseModel):
-    src: str
-    msg: str
+class Notification(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    src: str = Field(index=True)
+    msg: str = Field(index=True)
+    created_at: datetime
 
-app = FastAPI()
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+app = FastAPI(on_startup=[create_db_and_tables])
 
 @app.get('/')
 def read_root():
@@ -19,5 +38,17 @@ def read_item(id: int):
     return {"item": id }
 
 @app.post('/notify/')
-def create_noti(notification: Notification):
+def create_noti(notification: Notification, session: SessionDep) -> Notification:
+    session.add(notification)
+    session.commit()
+    session.refresh(notification)
     return notification
+
+@app.get('/messages/')
+def read_otp(
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+) -> list[Notification] :
+    noti = session.exec(select(Notification).offset(offset).limit(limit)).all()
+    return noti
